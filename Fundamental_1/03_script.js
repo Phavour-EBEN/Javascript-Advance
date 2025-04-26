e2c229f29743542315d71440211542eef6b5f6e0
Phavour-EBEN <ainooebenezer05@gmail.com>
2025-04-26 00:41:28 +0000

    adds a function lesson

---
+++ Fundamental_1/03_script.js
@@ -0,0 +1,26 @@
+// lession 1
+
+"strict";
+/*
+let hasDriversLicense = false;
+const hasGoodVision = true;
+if (hasGoodVision) hasDriversLicense = true;
+
+if (hasDriversLicense) console.log("Sarah is able to drive");
+
+
+
+// lession 2 functions
+
+
+function fruitDrinks(apples, oranges) {
+    //  apples and oranges are parameters
+  const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+  return juice;
+}
+
+// calling the function
+// note that 5 and 2 are arguments
+const appleJuice = fruitDrinks(5, 2);
+console.log(appleJuice);
+*/
