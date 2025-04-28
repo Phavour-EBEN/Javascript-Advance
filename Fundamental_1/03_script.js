5a42ffcb78c6bd16f900d8014f97910c2661dccc
Phavour-EBEN <ainooebenezer05@gmail.com>
2025-04-28 17:54:30 +0000

    completes the coding challeng 1 for chapter 3 part 1

--- Fundamental_1/03_script.js
+++ Fundamental_1/03_script.js
@@ -91,3 +91,52 @@ const yearUntilRetirement = function (fname){
 
 console.log(yearUntilRetirement("Jonas"));
 */
+// coding challenge 1
+/*
+Back to the two gymnastics the Koalas! There is a new
+Which works differently.Each team competes 3 times,
+the 3 scores is calculated team). A team ONLY wins if jt has
+average score of the other teams, the Dolphins and
+gymnastics discipline, and then the average of (so one average score per at least DOUBLE the team. Otherwåse, no team wins! 
+
+1. Create an arrow function 'ca!cAverage' to
+calculate the average of 3 scores
+2. use the function to calculate the average for both
+teams
+3. Create a function 'checkWinner' that takes the
+average score of each team as parameters
+('avgDotbjns' and 'avgKoalas'), and then logs the
+winner to the console, together with the victory
+points, according to the rule above. Example: "Koalas
+f 30 vs. 13)".
+4. Use the 'checkWinner' function to determine the
+winner for both DATA 1 and DATA 2.
+5. Ignore draws this time.
+
+Dolphins score 44,23,and 71
+Koalas score 65, 54, 49
+
+// solution
+
+const AvgScores = (a,b,c) => {
+    return (a + b + c)/3;  
+}
+const Dolphins = AvgScores(85,54, 41);
+const Koalas = AvgScores(23,534,27);
+
+console.log(`Dolphins avg score is ${Dolphins}`);
+console.log(`Koalass avg score is ${Koalas}`);
+
+const checkWinners = (Dolphins, Koalas) => {
+    if(Dolphins >=(Koalas*2)){
+        console.log(`Dolphins wins (${Dolphins} vs ${Koalas})`);
+    }else if(Koalas >=(Dolphins*2)){
+        console.log(`Koalas wins (${Koalas} vs ${Dolphins})`);
+    }else{
+        console.log("No Team Wins");
+    }
+}
+
+console.log(checkWinners(Dolphins, Koalas))
+
+*/
